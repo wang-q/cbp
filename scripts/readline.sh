16ae25152fe428f8296244999f9714d049838d0c
Qiang Wang <wang-q@users.noreply.github.com>
2025-03-02 08:31:49 +0800

    readline

---
+++ scripts/readline.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Source common build environment: extract source, setup dirs and functions
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+# Extract source code
+extract_source
+
+# ./configure --help
+
+# Build with the specified target architecture
+CC="zig cc -target ${TARGET_ARCH}" \
+CXX="zig c++ -target ${TARGET_ARCH}" \
+CFLAGS="-I$HOME/bin/include" \
+CXXFLAGS="-I$HOME/bin/include" \
+LDFLAGS="-L$HOME/bin/lib" \
+    ./configure \
+    --prefix="${TEMP_DIR}/collect" \
+    --bindir="${TEMP_DIR}/collect" \
+    --disable-dependency-tracking \
+    --disable-silent-rules \
+    --disable-shared \
+    --enable-static \
+    --disable-install-examples \
+    --with-curses \
+    || exit 1
+make -j 8 || exit 1
+make install || exit 1
+
+# tree "${TEMP_DIR}/collect"
+# ldd "${TEMP_DIR}/collect/bin/readline"
+
+# Use build_tar function from common.sh
+build_tar
