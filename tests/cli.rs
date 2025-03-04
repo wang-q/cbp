a4f0b35e2c3a1eb2bcee51a625837cf540750080
Qiang Wang <wang-q@users.noreply.github.com>
2025-03-04 21:48:58 +0800

    Update cli.rs

--- tests/cli.rs
+++ tests/cli.rs
@@ -20,7 +20,7 @@ fn command_kb_readme() -> anyhow::Result<()> {
     let stdout = String::from_utf8(output.stdout).unwrap();
 
     assert!(stdout.lines().count() > 10);
-    assert!(stdout.contains("bioinformatics tools"));
+    assert!(stdout.contains("bioinformatics"));
 
     Ok(())
 }
