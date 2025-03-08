0b34f33c8b8e45f6227d35ecf907353836368486
Qiang Wang <wang-q@outlook.com>
2025-03-08 09:11:04 +0800

    faops

---
+++ ports/faops/portfile.cmake
@@ -0,0 +1,25 @@
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/../../sources/faops.tar.gz" DESTINATION "${CURRENT_BUILDTREES_DIR}")
+
+# Suppress warning about empty include directory
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${CURRENT_BUILDTREES_DIR}/faops.tar.gz"
+)
+
+# Copy CMakeLists.txt to source directory
+configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" "${SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-DCMAKE_C_FLAGS=-O2"
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
