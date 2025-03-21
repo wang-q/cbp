2306ee5795749ad9426fa5ef0b6dba03c8ce302a
Qiang Wang <wang-q@users.noreply.github.com>
2025-03-22 00:10:16 +0800

    Update spoa.sh

--- test-binaries/spoa.sh
+++ test-binaries/spoa.sh
@@ -5,4 +5,4 @@ source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
 echo "==> Testing ${PROJ} installation"
 
 # Test version output
-test_version "spoa" "^[0-9]+\.[0-9]+\.[0-9]+$"
+test_version "spoa" "[0-9]+\.[0-9]+\.[0-9]+"
