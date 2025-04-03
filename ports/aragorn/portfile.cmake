fa8c4bc98b8f6a42d3bc6f8bd99d18dc60ff821b
Qiang Wang <wang-q@outlook.com>
2025-04-04 01:28:55 +0800

    aragorn

---
+++ ports/aragorn/portfile.cmake
@@ -0,0 +1,20 @@
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/../../sources/aragorn.tar.gz" DESTINATION "${CURRENT_BUILDTREES_DIR}")
+
+set(VCPKG_POLICY_EMPTY_INCLUDE_FOLDER enabled)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${CURRENT_BUILDTREES_DIR}/aragorn.tar.gz"
+)
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" "${SOURCE_PATH}/CMakeLists.txt" COPYONLY)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
